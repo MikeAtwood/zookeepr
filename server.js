6ef3d4c44a2d435f3cf708fe09dc99daa32f1d7b
Deadpool1 <atwood169@yahoo.com>
2022-07-04 13:02:02 -0500

    added back tics

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 app.listen(PORT, () => {
-    console.log('API server now on port ${PORT}!');
+    console.log(`API server now on port ${PORT}!`);
 });
